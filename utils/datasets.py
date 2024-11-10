c1426a30d30537cb826c0a38dac580030123559c
Zhouyi-Zura <43192723+Zhouyi-Zura@users.noreply.github.com>
2024-11-10 17:09:12 +0800

    Add files via upload

--- utils/datasets.py
+++ utils/datasets.py
@@ -1,9 +1,9 @@
 import glob, sys
 import numpy as np
 from torch.utils.data import Dataset
-from PIL import Image
 import torchvision.transforms as transforms
-import skimage
+import cv2
+from PIL import Image
 
 
 class ImageDataset_Train(Dataset):
@@ -16,48 +16,36 @@ class ImageDataset_Train(Dataset):
 
     def add_noise(self, image, noise_mode):
         # -----------
-        # Gau0.02 Gau0.03 Gau0.04 Gau0.05 Gau0.06 Gau0.07 Gau0.08 Gau0.09
-        # GP0.02 GP0.03 GP0.04 GP0.05 GP0.06 GP0.07 GP0.08 GP0.09
+        # Ray10 Ray20 Ray30 Ray40 Ray50 Ray60 Ray70 Ray80
+        # RP10 RP20 RP30 RP40 RP50 RP60 RP70 RP80
         # -----------
-        n_mode = noise_mode[:-4]
-        Gau_var = noise_mode[-4:]
-        if n_mode == "Gau":
-            img = np.array(image)
-            img = skimage.util.random_noise(img, mode='gaussian', var = float(Gau_var))
-            noise_img = 255 * (img / np.amax(img))
-        elif n_mode == "GP":
-            img = np.array(image)
-            img = skimage.util.random_noise(img, mode='gaussian', var = float(Gau_var))
-            img = 255 * (img / np.amax(img))
-            noise = np.random.poisson(img)
-            noise_img = img + noise
-            noise_img = 255 * (noise_img / np.amax(noise_img))
+        n_mode = noise_mode[:-2]
+        Ray_sigma = noise_mode[-2:]
+        if n_mode == "Ray":
+            noise = np.random.rayleigh(scale=int(Ray_sigma), size=image.shape)
+            noise_img = image + noise
+            noise_img = np.clip(noise_img, 0, 255).astype(np.uint8)
+        elif n_mode == "RP":
+            noise = np.random.rayleigh(scale=int(Ray_sigma), size=image.shape)
+            noise_p = np.random.poisson(image)
+            noise_img = image + noise + noise_p
+            noise_img = np.clip(noise_img, 0, 255).astype(np.uint8)
         
         noisy_PIL = Image.fromarray(noise_img)
 
         return noisy_PIL
 
     def __getitem__(self, index):
-        img = Image.open(self.image_files[index % len(self.image_files)]).convert("L")
-        lab = Image.open(self.label_files[index % len(self.label_files)]).convert("L")
+        img = cv2.imread(self.image_files[index % len(self.image_files)], cv2.IMREAD_GRAYSCALE)
+        lab = cv2.imread(self.label_files[index % len(self.label_files)], cv2.IMREAD_GRAYSCALE)
 
-        if (np.random.random() < 0.5):
-            img = img.transpose(Image.FLIP_LEFT_RIGHT)
-            lab = lab.transpose(Image.FLIP_LEFT_RIGHT)
-
-        if self.ML_mode == 0:
-            factor = np.random.randint(-15,15)
-            img = img.rotate(factor)
-            lab = lab.rotate(factor)
-            img = self.transform(img)
-            lab = self.transform(lab)
-        elif self.ML_mode == 1:
+        if self.ML_mode == 1:
             img = self.add_noise(lab, self.task_flag)
-            factor = np.random.randint(-15,15)
-            img = img.rotate(factor)
-            lab = lab.rotate(factor)
             img = self.transform(img)
-            lab = self.transform(lab)
+            lab = self.transform(Image.fromarray(lab))
+        elif self.ML_mode == 0:
+            img = self.transform(Image.fromarray(img))
+            lab = self.transform(Image.fromarray(lab))
         else:
             sys.exit("Wrong Meta-Learning mode!")
 
